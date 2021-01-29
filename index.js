f91266c363b7ff9d4698ffba80ef036a49a8880b
04zAM <pathob13@gmail.com>
2021-01-28 22:38:53 -0500

    Segunda fase de cambiar sequelize por pools

--- index.js
+++ index.js
@@ -1,58 +1,32 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.json());
-const sequelize = new Sequelize('postgres://vbbgvldluphduq:e97b2f2dd12306ac327274f3164a4c8394eef89e11500850c417f98f8ea098f7@ec2-52-44-46-66.compute-1.amazonaws.com:5432/d8tl0oip7rspjp')
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
-
-//Model
-const User = sequelize.define(
-  "user",
-  {
-    // attributes
-    firstName: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    lastName: {
-      type: Sequelize.STRING,
-      // allowNull defaults to true
-    },
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
   },
-  {
-    // options
-  }
-);
-// Note: using `force: true` will drop the table if it already exists
-User.sync({ force: true }); // Now the `users` table in the database corresponds to the model definition
-app.get("/", (req, res) => res.json({ message: "Hello World" }));
-app.post("/user", async (req, res) => {
-  try {
-    const newUser = new User(req.body);
-    await newUser.save();
-    res.json({ user: newUser }); // Returns the new user that is created in the database
-  } catch (error) {
-    console.error(error);
-  }
 });
-app.get("/user/:userId", async (req, res) => {
-  const userId = req.params.userId;
-  try {
-    const user = await User.findAll({
-      where: {
-        id: userId,
-      },
-    });
-    res.json({ user });
-  } catch (error) {
-    console.error(error);
-  }
-});
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app
+  .use(express.json())
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .get("/users", async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM usuarios;");
+      const results = json(result)//{ results: result ? result.rows : null };
+      res.render("pages/users", results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
